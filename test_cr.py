9e23297b01324a6545670269bf1c704dd0e3bac9
Dmitry Ilyashenko <dvil@mail.ru>
2019-09-26 20:44:01 +0400

    adding test

---
+++ test_cr.py
@@ -0,0 +1,23 @@
+import pytest
+import asyncio
+from config import CR_CONFIG
+from drivers.atol10_adapter import Atl_cash_register
+
+
+pytestmark=pytest.mark.asyncio
+
+@pytest.yield_fixture
+async def connect_to_driver(event_loop):
+    cash_register=Atl_cash_register(**CR_CONFIG, loop=event_loop)
+    try:
+        yield cash_register
+    finally:
+        await cash_register.close()
+        
+        
+class TestCR:
+    def test_first(self, connect_to_driver, event_loop):
+         asyncio.sleep(5, loop=event_loop)
+         
+         
+ 
