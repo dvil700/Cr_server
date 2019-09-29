406b34b558494b45a320b0965681ad01a8a30d45
Dmitry Ilyashenko <dvil@mail.ru>
2019-09-29 16:04:29 +0400

    fixing adapter

--- drivers/atol10_adapter.py
+++ drivers/atol10_adapter.py
@@ -314,7 +314,7 @@ class Atl_cash_register(Cash_register_interface):
        #ссылка с подробной информацией о возвратах и коррекциях выше
         if r1192: 
              self._setParam(1192, r1192)
-
+        self._openReceipt()
        # if self._openReceipt()<0:
         #     self.driver.cancelReceipt()
         #     raise CROperationError(self.name, 'Ошибка открытия чека: ' + self._errorDescription())
