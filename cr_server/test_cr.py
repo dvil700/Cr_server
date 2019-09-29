5352593271577e016678f84ec7edf91052898686
Dmitry Ilyashenko <dvil@mail.ru>
2019-09-29 22:41:52 +0400

    new structure

---
+++ cr_server/test_cr.py
@@ -0,0 +1,39 @@
+import pytest
+import asyncio
+from config import CR_CONFIG
+from drivers.atol10_adapter import Atl_cash_register
+
+
+pytestmark=pytest.mark.asyncio
+
+@pytest.yield_fixture
+async def connect_to_driver(event_loop):
+    cash_register=Atl_cash_register(**CR_CONFIG, loop=event_loop)
+    try:
+        yield cash_register
+    finally:
+        await cash_register.close()
+        
+        
+class TestCR:
+    async def test_first(self, connect_to_driver, event_loop):
+        driver= connect_to_driver
+        data= dict(email='ddnjjk@locald.ru',
+                   products=[{'name': 'Поилка', 'price': 40, 'quantity': 1.0, 'paymentObject':1, 'paymentMethod':1},
+                             {'name': 'Поилка вакуумная', 'price': 40, 'quantity': 1.0, 'paymentObject':1, 'paymentMethod':1}],
+                   payments=[{'summ':80, 'payment_type':0}], total=80, receiptType=1,
+                   operator = {'name':'Ильяшенко Дмитрий Владимирович', 'inn':'344405326900'}, is_electronary = False,
+                   r1192 = None, test_mode = True)
+        try:
+            result = await driver.register_operation(*data.values())
+            assert result==0, result
+        except Exception as e:
+            driver.driver.cancelReceipt()
+            assert False, e
+
+
+
+
+
+         
+ 
